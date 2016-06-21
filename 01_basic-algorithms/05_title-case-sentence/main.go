d384db9d892c4e65b22084fd54ad704c7cd81ba8
robjloranger <robjloranger@gmail.com>
2016-06-20 18:32:48 -0700

    almost working

--- 01_basic-algorithms/05_title-case-sentence/main.go
+++ 01_basic-algorithms/05_title-case-sentence/main.go
@@ -9,8 +9,21 @@
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 	sentence1 := "Hello there world"
 	sentence2 := "I'm A liTTlE teA Pot"
 	sentence3 := "SHORT AND STOUT"
+	fmt.Println(titlecase(sentence1))
+	fmt.Println(titlecase(sentence2))
+	fmt.Println(titlecase(sentence3))
+}
+
+func titlecase(s string) string {
+	s = strings.ToLower(s)
+	return strings.Title(s)
 }
