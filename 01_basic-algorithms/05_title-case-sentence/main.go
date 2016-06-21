f94b2ad47f717d7c6bcbf600b222f6d898b7d211
robjloranger <robjloranger@gmail.com>
2016-06-20 18:17:53 -0700

    structure and description

---
+++ 01_basic-algorithms/05_title-case-sentence/main.go
@@ -0,0 +1,16 @@
+/*
+	Basic Algorithms
+	05 Title Case A Sentence
+
+	Program returns a sentence, title cased
+
+	Written by: robjloranger
+*/
+
+package main
+
+func main() {
+	sentence1 := "Hello there world"
+	sentence2 := "I'm A liTTlE teA Pot"
+	sentence3 := "SHORT AND STOUT"
+}
