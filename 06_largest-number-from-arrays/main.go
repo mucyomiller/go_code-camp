e2f65459255ced17ecbc1f66361094a366261811
robjloranger <robjloranger@gmail.com>
2016-06-24 07:11:22 -0700

    working out one more bug

--- 06_largest-number-from-arrays/main.go
+++ 06_largest-number-from-arrays/main.go
@@ -14,14 +14,35 @@ package main
 import "fmt"
 
 func main() {
+	// define some test data
 	array1 := []int{2, 30, 86, 12}
 	array2 := []int{0, 31, 126, 76, 45, 22, 90, 86}
+	// execute our function a few ways
 	fmt.Println(largest(array1))
+	fmt.Println(largest(array1, array2))
+	fmt.Println(largest(array1, array2, array1))
 }
 
-func largest(arrays ...[]int) int {
+// function to return array of largest numbers
+func largest(arrays ...[]int) []int {
+	// variadic input
+	// create empty array length of input
+	output := make([]int, len(arrays))
+	// loop over input
 	for arr := range arrays {
-		fmt.Println(arr)
+		// new empty highest number
+		var highest int
+		// loop over this array of numbers
+		// BUG: Error, invalid input for len.
+		// arr type int.. what?
+		for x := 0; x < len(arr)-1; x++ {
+			// keep highest up to date
+			if x > highest {
+				highest = x
+			}
+		}
+		// add highest to array
 	}
-	return 1
+	// return new array
+	return output
 }
