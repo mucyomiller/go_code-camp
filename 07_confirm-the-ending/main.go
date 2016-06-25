68df4e346621df1e03644ed094c47ba5447f8485
robjloranger <robjloranger@gmail.com>
2016-06-24 17:44:01 -0700

    working code for confirm the ending

--- 07_confirm-the-ending/main.go
+++ 07_confirm-the-ending/main.go
@@ -24,6 +24,16 @@ func main() {
 	fmt.Printf("The string '%v' ends in '%v' == %v\n", string2, target1, confirm(string2, target1))
 }
 
-func confirm(str, target) bool {
-
+func confirm(str string, target string) bool {
+	// loop over length of target
+	for x := len(target); x > 0; x-- {
+		// checking if rune at string position -x is equal
+		// to rune at target position -x
+		if str[len(str)-x] != target[len(target)-x] {
+			// if not equal, return false
+			return false
+		}
+	}
+	// otherwise return true to confirm the ending
+	return true
 }
