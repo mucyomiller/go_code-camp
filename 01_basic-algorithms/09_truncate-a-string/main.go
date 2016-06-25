78421396bc58559c49f3ee903ee67ce9ba882b3e
robjloranger <robjloranger@gmail.com>
2016-06-24 19:15:30 -0700

    working away

--- 01_basic-algorithms/09_truncate-a-string/main.go
+++ 01_basic-algorithms/09_truncate-a-string/main.go
@@ -11,8 +11,18 @@
 
 package main
 
+import "fmt"
+
 func main() {
 	// some test data
 	string1 := "The quick brown fox"
 	string2 := "hi"
+	// call truncate tests
+	fmt.Println(truncate(string1, 8))
+	fmt.Println(truncate(string1, 2))
+}
+
+// truncate function, returns a new string
+func truncate(str string, x int) string {
+	// stuff goes here
 }
