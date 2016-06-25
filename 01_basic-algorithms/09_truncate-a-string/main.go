d8a324c8ad997fa1b143edf6074bbf937b803c0e
robjloranger <robjloranger@gmail.com>
2016-06-24 18:44:44 -0700

    starting declarations

--- 01_basic-algorithms/09_truncate-a-string/main.go
+++ 01_basic-algorithms/09_truncate-a-string/main.go
@@ -2,5 +2,17 @@
 	Basic Algorithms
 	09 Truncate A String
 
+	Returns a string truncated at the provided length
+	ending in '...', the length is inclusive of the dots
+	unles the string length is == or < 3.
+
 	Written by: robjloranger
 */
+
+package main
+
+func main() {
+	// some test data
+	string1 := "The quick brown fox"
+	string2 := "hi"
+}
