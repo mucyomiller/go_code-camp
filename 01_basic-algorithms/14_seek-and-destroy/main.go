ac2b305aa53aca45cab8414db2ab7cf261d86c81
robjloranger <robjloranger@gmail.com>
2016-06-26 11:04:46 -0700

    layed out seek and destroy

---
+++ 01_basic-algorithms/14_seek-and-destroy/main.go
@@ -0,0 +1,30 @@
+/*
+	Basic Algorithms
+	14 Seek And Destroy
+
+	This program removes one or more int's from a slice
+	of int.
+
+	Written by: robjloranger
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// data
+	data := []int{4, 5, 2, 4, 2, 1, 5, 7, 8, 9, 7, 6}
+	// tests
+	// just setup, not functioning
+	fmt.Printf("Data: %v\nDestroy: %v\nResults: %v\n", data, []int{2, 4, 9}, "n/a")
+}
+
+// TODO: write this function
+// destructive function
+func destroy(input []int, remove ...[]int) []int {
+	// new output slice of int
+	var output []int
+	// return output
+	return output
+}
