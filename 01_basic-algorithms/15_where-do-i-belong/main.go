52e686a637a23356272cea8f7fc6501ef765e0b2
robjloranger <robjloranger@gmail.com>
2016-06-27 18:15:52 -0700

    setup

---
+++ 01_basic-algorithms/15_where-do-i-belong/main.go
@@ -0,0 +1,9 @@
+/*
+	Basic Algorithms
+	15 Where Do I Belong
+
+	This program determines the index at which an integer
+	should be placed in a slice after being sorted.
+
+	Written by: robjloranger
+*/
