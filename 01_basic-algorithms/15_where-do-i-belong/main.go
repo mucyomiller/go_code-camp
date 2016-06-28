b8527dafff14c42bd6985e26ede2916305357718
robjloranger <robjloranger@gmail.com>
2016-06-27 21:13:54 -0700

    layed out function signature and tests

--- 01_basic-algorithms/15_where-do-i-belong/main.go
+++ 01_basic-algorithms/15_where-do-i-belong/main.go
@@ -7,3 +7,21 @@
 
 	Written by: robjloranger
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	// data
+	numbers := []float64{10, 20, 30, 40, 50}
+	// tests
+	test1idx, test1full := ibelong(numbers, 35)
+	fmt.Printf("If we sort %v, and want to add %v it should be in index %v.\nLike this: %v\n", numbers, 35, test1idx, test1full)
+	test2idx, test2full := ibelong(numbers, 10.5)
+	fmt.Printf("If we sort %v, and want to add %v it should be in index %v.\nLike this: %v\n", numbers, 10.5, test2idx, test2full)
+}
+
+func ibelong(friends []float64, newbie float64) (int, []float64) {
+	return 2, friends
+}
