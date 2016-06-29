4ce78b7b9c2f09eb632325d6d8a07071c96c0dc2
robjloranger <robjloranger@gmail.com>
2016-06-28 18:35:04 -0700

    fixed title case bug

--- 01_basic-algorithms/05_title-case-sentence/main.go
+++ 01_basic-algorithms/05_title-case-sentence/main.go
@@ -15,16 +15,40 @@ import (
 )
 
 func main() {
+	// data to test
 	sentence1 := "Hello there world"
 	sentence2 := "I'm A liTTlE teA Pot"
 	sentence3 := "SHORT AND STOUT"
+	// call titlecase on our test data
 	fmt.Println(titlecase(sentence1))
 	fmt.Println(titlecase(sentence2))
 	fmt.Println(titlecase(sentence3))
 }
 
+// function to title case a string by words
+// i.e. each word title cased
 func titlecase(s string) string {
+	// start by lower casing the whole string
 	s = strings.ToLower(s)
-	// TODO: write custom map to title case the string
-	return strings.Title(s) // BUG: Title mishandles punctuation
+	// set the previous to a space " " so the
+	// first word gets title cased
+	prev := " "
+	// init empty output
+	var output string
+	// loop over input string
+	for _, c := range s {
+		// if previous character was a space and this
+		// character is alpha
+		if prev == " " && rune(c) >= 97 && rune(c) <= 122 {
+			// convert to upper case
+			output += string(rune(c) - 32)
+		} else {
+			// else just append to output
+			output += string(c)
+		}
+		// set prev to the last character iterated over
+		prev = string(c)
+	}
+	// return new output string
+	return output
 }
