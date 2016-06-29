88000f4679d217c75c16b138f2ca416cf78fb648
robjloranger <robjloranger@gmail.com>
2016-06-29 06:50:44 -0700

    header comment

---
+++ 02_intermediate-algorithms/02_diff-two-arrays/main.go
@@ -0,0 +1,11 @@
+/*
+	Intermediate Algorithms
+	02 Diff Two Arrays
+
+	This program returns the symetric difference
+	of two slices. A new slice with values found
+	in one slice but not the other.
+
+	Written by: robjloranger
+*/
+
