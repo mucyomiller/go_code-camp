2f87f45cbcc842a2e4cbfaddcb9caf4d6ae8fcfa
robjloranger <robjloranger@gmail.com>
2016-07-18 16:59:13 -0700

    comments

--- 01_basic-algorithms/13_falsey-bouncer/main.go
+++ 01_basic-algorithms/13_falsey-bouncer/main.go
@@ -1,3 +1,15 @@
+/*
+	Basic Algorithms
+	13 Falsey Bouncer
+
+	This program takes an 'array' of mixed values,
+	actually a slice of empty interface, and returns
+	a new slice with only the values that would be
+	true in Javascript
+
+	Written by: robjloranger
+*/
+
 package main
 
 import "fmt"
@@ -9,9 +21,14 @@ func main() {
 	fmt.Println(bounced)
 }
 
+// function to create a new slice with only the
+// 'true' values
 func bounce(a []interface{}) []interface{} {
 	var output []interface{}
 	for _, v := range a {
+		// TODO: try a switch here on type
+		// to check for 'false' for different types
+		// replicating javascript behaviour
 		if v == true {
 			output = append(output, v)
 		}
